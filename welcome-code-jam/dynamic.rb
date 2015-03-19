59562212eb0a58880ff8d2de051117b462fc1036
Kevin Tung <kekekevin@gmail.com>
2015-03-18 20:53:48 -0500

    welcome to code jam large and small tests passing

--- welcome-code-jam/dynamic.rb
+++ welcome-code-jam/dynamic.rb
@@ -1,9 +1,7 @@
 class Dynamic
 
 	def initialize
-		@input_string = "wweellccoommee to code qps jam"
 		@search_string = "welcome to code jam"
-		@result_table = Array.new(@input_string.length) { Array.new(@search_string.length) }
 	end
 
 	def get_value(x, y)
@@ -19,7 +17,7 @@ class Dynamic
 			@search_string.each_char.with_index do | search_char, y |
 				if input_char == search_char
 					if y == 0
-						@result_table[x][y] = 1 + get_value(x-1, 0)
+						@result_table[x][y] = 1 + get_value(x - 1, 0)
 					else
 						@result_table[x][y] = get_value(x - 1, y - 1) + get_value(x - 1, y)
 					end
@@ -30,16 +28,25 @@ class Dynamic
 		end 
 	end
 
-	def print
-		@result_table.each_with_index do | row, x |
-			row.each_with_index do | val, y |
-				puts "#{x} #{y} #{val}"
-			end
-		end
+	def result(input_string)
+		@input_string = input_string
+		@result_table = Array.new(@input_string.length) { Array.new(@search_string.length) }
+
+		self.populate_result_table
+
+		@result_table[-1][-1]
 	end
 
 end
 
 dynamic = Dynamic.new
-dynamic.populate_result_table
-dynamic.print
+in_file = File.open("C-large-practice.in", "r")
+num_inputs = in_file.readline
+out_file = File.open("practice.out", "w")
+
+(1..num_inputs.to_i).each do |i|
+	out_file.puts "Case \##{i}: #{dynamic.result(in_file.readline).to_s.rjust(4, '0')[-4..-1]}"
+end
+
+out_file.close
+in_file.close
