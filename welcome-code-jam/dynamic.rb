4eef7ff66b34b0e134c52dd19e1e4c4fbf0cf980
Kevin Tung <kekekevin@gmail.com>
2015-03-18 21:24:38 -0500

    reformat

--- welcome-code-jam/dynamic.rb
+++ welcome-code-jam/dynamic.rb
@@ -1,41 +1,41 @@
 class Dynamic
 
-	def initialize
-		@search_string = "welcome to code jam"
-	end
+  def initialize
+    @search_string = "welcome to code jam"
+  end
 
-	def get_value(x, y)
-		if x < 0 || y < 0
-			0
-		else
-			@result_table[x][y]
-		end
-	end
+  def get_value(x, y)
+    if x < 0 || y < 0
+		  0
+    else
+      @result_table[x][y]
+    end
+  end
 
-	def populate_result_table
-		@input_string.each_char.with_index do | input_char, x |
-			@search_string.each_char.with_index do | search_char, y |
-				if input_char == search_char
-					if y == 0
-						@result_table[x][y] = 1 + get_value(x - 1, 0)
-					else
-						@result_table[x][y] = get_value(x - 1, y - 1) + get_value(x - 1, y)
-					end
-				else
-					@result_table[x][y] = get_value(x - 1, y)
-				end
-			end
-		end 
-	end
+  def populate_result_table
+    @input_string.each_char.with_index do | input_char, x |
+      @search_string.each_char.with_index do | search_char, y |
+        if input_char == search_char
+          if y == 0
+            @result_table[x][y] = 1 + get_value(x - 1, 0)
+          else
+            @result_table[x][y] = get_value(x - 1, y - 1) + get_value(x - 1, y)
+				  end
+        else
+          @result_table[x][y] = get_value(x - 1, y)
+        end
+      end
+    end
+  end
 
-	def result(input_string)
-		@input_string = input_string
-		@result_table = Array.new(@input_string.length) { Array.new(@search_string.length) }
+  def result(input_string)
+    @input_string = input_string
+    @result_table = Array.new(@input_string.length) { Array.new(@search_string.length) }
 
-		self.populate_result_table
+    self.populate_result_table
 
-		@result_table[-1][-1]
-	end
+    @result_table[-1][-1]
+  end
 
 end
 
@@ -45,7 +45,7 @@ num_inputs = in_file.readline
 out_file = File.open("practice.out", "w")
 
 (1..num_inputs.to_i).each do |i|
-	out_file.puts "Case \##{i}: #{dynamic.result(in_file.readline).to_s.rjust(4, '0')[-4..-1]}"
+  out_file.puts "Case \##{i}: #{dynamic.result(in_file.readline).to_s.rjust(4, '0')[-4..-1]}"
 end
 
 out_file.close
