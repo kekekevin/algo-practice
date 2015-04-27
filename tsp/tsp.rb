49d7cfc828a346a989a47f40a8293a018981d6a4
Kevin Tung <kekekevin@gmail.com>
2015-04-27 18:57:58 -0500

    tsp solved for examples

--- tsp/tsp.rb
+++ tsp/tsp.rb
@@ -15,7 +15,7 @@ class Tsp
           distances[i][j] = Math.sqrt((points[i][:x] - points[j][:x]) ** 2 + (points[i][:y] - points[j][:y]) ** 2)
         end
       end
-      solve(points, distances)
+      puts solve(points, distances)
     end
   end
 
@@ -32,33 +32,35 @@ class Tsp
     end
 
     1.upto(num_points) do |m|
-      puts "#{table}"
       1.upto(2 ** (num_points - 1) - 1) do |s|
         subset = "%#{2 ** (num_points - 1)}b" % s
         if subset.count("1") == m
           0.upto(num_points - 1) do |j|
             if subset[-j - 1] == "1"
               if m == 1
-                puts subset
-                table[s - 1][j + 1] = distances[0][j + 1]
+                table[s][j + 1] = distances[0][j + 1]
               else
                 x = String.new(subset)
                 x[-j - 1] = "0"
-                puts "subset #{subset}"
-                puts "#{x}"
-                table[s - 1][j + 1] = table[x.to_i(2)].each_with_index.collect do |e, k|
+                # puts "subset #{subset}"
+                # puts "#{x}"
+                table[s][j + 1] = table[x.to_i(2)].each_with_index.collect do |e, k|
                   e + distances[k][j + 1]
                 end.min
               end
             end
-            # find subset with j taken out
-            # table[s - 1][j]
           end
         end
       end
     end
-    puts "#{table}"
+    # table.each_with_index do |t, i|
+      # puts "#{i} #{t}"
+    # end
+    table[-1].each_with_index.collect do |cost, j|
+      cost + distances[j][0]
+    end.min
   end
 end
 
 Tsp.new("example.in")
+Tsp.new("example2.in")
